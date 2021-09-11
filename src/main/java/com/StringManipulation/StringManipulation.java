88fb541f3780bfdf20ac6e992c921447bf23c1ba
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-09-11 17:11:39 +0200

    string algos

--- src/main/java/com/StringManipulation/StringManipulation.java
+++ src/main/java/com/StringManipulation/StringManipulation.java
@@ -1,12 +1,12 @@
 package com.StringManipulation;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import com.example.ArrayQueue;
+
+import java.util.*;
 
 public class StringManipulation {
 
-    public int countVowels(String input) {
+    public static int countVowels(String input) {
         HashSet<String> vowels = new HashSet<>(Arrays.asList("a", "e", "i", "o", "u"));
         int count = 0;
         for(char c : input.toCharArray()) {
@@ -15,5 +15,48 @@ public class StringManipulation {
         }
      return count;
     }
+    public static String stringReverser(String input) {
+        if (input == null) return "";
+     StringBuffer reversed = new StringBuffer();
+     for (int i = input.length()-1; i >= 0 ; i--)
+         reversed.append(input.charAt(i));
+     return reversed.toString();
+    }
 
+    public boolean areAnagrams(String input, String input2) {
+        if (input == null || input2 == null || input.length() != input2.length()) return false;
+        char[] array1 = input.toCharArray();
+        Arrays.sort(array1);
+        char[] array2 = input2.toCharArray();
+        Arrays.sort(array2);
+        return Arrays.equals(array1, array2);
+    }
+    public static boolean isPalindrome(String word){
+        int first = 0;
+        int last = word.length()-1;
+
+        while(first < last)
+            if(word.charAt(first++) != word.charAt(last--))
+                return false;
+
+        return true;
+    }
+
+    public static char mostRepeatedCharacter(String input){
+        if(input.length() == 0) throw new IllegalStateException();
+
+        final int ALPHABET = 256;
+        int[] frecuencies =  new int[ALPHABET];
+        for(char c : input.toCharArray())
+            frecuencies[c]++;
+        int max = 0;
+        char result = ' ';
+        for (int i = 0; i < frecuencies.length; i++)
+            if(frecuencies[i] > max ){
+                max = frecuencies[i];
+                        result = (char) i;}
+
+         return result;
+
+    }
 }
--- src/test/java/com/example/StringManipulationTest.java
+++ src/test/java/com/example/StringManipulationTest.java
@@ -3,14 +3,38 @@ package com.example;
 import com.StringManipulation.StringManipulation;
 import org.junit.Test;
 
+import static com.StringManipulation.StringManipulation.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StringManipulationTest {
 
-    @Test public void testThatCanRetrieveNumberOfVowels(){
-        StringManipulation manipulator = new StringManipulation();
-        int result = manipulator.countVowels("hello");
+    @Test
+    public void testThatCanRetrieveNumberOfVowels() {
+
+        int result = countVowels("hello");
         assertEquals(2, result);
 
     }
+    @Test
+    public void testStringReverser() {
+        String result = stringReverser("hello");
+        assertEquals("olleh", result);
+    }
+
+    @Test public void testIsPalindrome(){
+
+        boolean result = isPalindrome("madam");
+        assertTrue(result);
+
+    }
+
+    @Test public void testMostRepeatedCharacters() {
+        char result = mostRepeatedCharacter("pppppppppppppppppppppppppppaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaappppppppppppppppppppppprrrrrrrrrrrrrrrrr");
+        assertEquals('p',result );
+    }
+
+
+
+
 }
